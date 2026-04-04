6ffb2dbcdc286956a3f048aab0abb96263f6ac32
Nawin chander <nawinnapzy289@gmail.com>
2026-04-04 15:33:06 +0530

    add code

--- Test/Reconnection & Reliability Test.js
+++ Test/Reconnection & Reliability Test.js
@@ -6,3 +6,24 @@
 // Session recovery
 // Message replay (Kafka)
 
+const WebSocket = require('ws');
+
+function connect() {
+    const ws = new WebSocket('ws://localhost:8080');
+
+    ws.on('open', () => {
+        console.log('Connected');
+    });
+
+    ws.on('close', () => {
+        console.log('Disconnected. Reconnecting...');
+        setTimeout(connect, 1000);
+    });
+
+    ws.on('error', () => {
+        ws.close();
+    });
+}
+
+connect();
+
