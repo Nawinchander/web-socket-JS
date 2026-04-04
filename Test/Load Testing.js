87d38e99baaede3a932e8bec4c43aed2b4a7d6a1
Nawin chander <nawinnapzy289@gmail.com>
2026-04-04 15:30:45 +0530

    add

---
+++ Test/Load Testing.js
@@ -0,0 +1,13 @@
+//// Load Testing (Simulating 10,000 Users)
+
+// FAANG Insight:
+// Mention tools like:
+// k6
+// Artillery
+
+// Talk about:
+// connection limits per node
+// epoll / event loop scaling
+
+
+
