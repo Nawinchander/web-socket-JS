ec89c056f59b373e444ba2dda16eb2c90ee3542c
Nawin chander <nawinnapzy289@gmail.com>
2026-04-04 15:30:03 +0530

    add

--- Test/Functional Test.js
+++ Test/Functional Test.js
@@ -5,3 +5,23 @@
 // Handle JSON schema validation
 // Mention contract testing
 
+const WebSocket = require('ws');
+
+const ws = new WebSocket('ws://localhost:8080');
+
+ws.on('open', () => {
+    console.log('Connected');
+
+    ws.send(JSON.stringify({ type: 'PING' }));
+});
+
+ws.on('message', (data) => {
+    const msg = JSON.parse(data);
+    
+    if (msg.type === 'PONG') {
+        console.log('✅ Functional Test Passed');
+    } else {
+        console.log('❌ Unexpected response');
+    }
+});
+
