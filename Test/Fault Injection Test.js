8cbfd6125eff48441e04fb4d72950a613751705b
Nawin chander <nawinnapzy289@gmail.com>
2026-04-04 15:35:24 +0530

    add

---
+++ Test/Fault Injection Test.js
@@ -0,0 +1,12 @@
+//// Fault Injection Test (Server Crash Simulation)
+
+// FAANG Insight:
+// Discuss:
+// At-least-once vs exactly-once delivery
+// Idempotency keys
+// Distributed recover
+
+
+
+
+
